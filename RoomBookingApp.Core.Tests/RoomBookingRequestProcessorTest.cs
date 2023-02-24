d8f2d9b4901cf1506c873b038105f0dff76d41ec
Dava2788 <dava2788@gmail.com>
2023-02-24 11:39:53 -0600

    Add project files.

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,38 @@
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using Shouldly;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        {
+            //Arrrange
+            var request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2023, 02, 24)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+            //Assert.NotNull(result);
+            //Assert.Equal(request.FullName, result.FullName);
+            //Assert.Equal(request.Email, result.FullName);
+            //Assert.Equal(request.Date, result.Date);
+
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(request.FullName);
+            result.Email.ShouldBe(request.Email);
+            result.Date.ShouldBe(request.Date);
+
+        }
+    }
+}
---
+++ RoomBookingApp.Core.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace RoomBookingApp.Core.Tests
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+        //    //Arrrange
+        //    //We're going to create objects
+        //    //or representations of whatever data we would expect
+        //    //to be testing against.
+
+        //    //Act
+        //    //Simulated Method to be call
+
+        //    //Assert
+        //    //Check if the test was successfully
+
+        //}
+    }
+}
